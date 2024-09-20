0f313085b028eb5ec3a2ff1986b6fb4fd50a04a3
David Zager <dzager@redhat.com>
2024-09-20 13:45:26 -0400

    Better mobile nav
    Signed-off-by: David Zager <dzager@redhat.com>

---
+++ app/components/navbar.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+
+export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <nav className="relative">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link href="/" className="flex items-center space-x-4 rtl:space-x-reverse">
+          <Image
+            src="/zzzbees.png"
+            alt="Zzz Bees Logo"
+            width={200}
+            height={200}
+            className="rounded-lg"
+          />
+        </Link>
+        <button 
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          type="button" 
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-black rounded-lg md:hidden hover:bg-yellow-100 focus:outline-none focus:ring-2 focus:ring-yellow-200"
+          aria-controls="navbar-default" 
+          aria-expanded={isMenuOpen}
+        >
+          <span className="sr-only">Toggle menu</span>
+          <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+          </svg>
+        </button>
+        <div className={`${isMenuOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
+
+            {/* <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"></ul> */}
+            <ul className="md:flex md:space-x-6">
+                <li><Link href="/about" className="text-black hover:text-yellow-600 text-lg font-bold">About Us</Link></li>
+                <li><Link href="/store" className="text-black hover:text-yellow-600 text-lg font-bold">Store</Link></li>
+            </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Image from "next/image";
-import Link from "next/link";
 import "./globals.css";
+import Navbar from "./components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,25 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} min-h-screen flex flex-col`}>
         <header className="bg-white shadow-md">
-          <div className="container mx-auto px-4 py-2 flex items-center justify-between">
-            <div className="flex items-center space-x-4">
-              <Link href="/" className="flex items-center space-x-4">
-                <Image
-                  src="/zzzbees.png"
-                  alt="Zzz Bees Logo"
-                  width={200}
-                  height={200}
-                  className="rounded-lg"
-                />
-              </Link>
-            </div>
-            <nav>
-              <ul className="flex space-x-6">
-                <li><Link href="/about" className="text-black hover:text-yellow-600 text-lg font-bold">About Us</Link></li>
-                <li><Link href="/store" className="text-black hover:text-yellow-600 text-lg font-bold">Store</Link></li>
-              </ul>
-            </nav>
-          </div>
+          <Navbar />
         </header>
         <main className="flex-grow container mx-auto px-4 py-8">
           {children}
